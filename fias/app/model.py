c4b64a1a04eb8d4ca37a9ef56039c0ac491971cd
Иван <ivanator666@gmail.com>
2025-05-20 16:24:37 +0300

    finished models

--- fias/app/model.py
+++ fias/app/model.py
@@ -1,140 +1,85 @@
-from sqlmodel import SQLModel, Field, create_engine, Session
-from typing import Optional
+from sqlmodel import SQLModel, Field, ARRAY, Column, Integer
+from typing import List
 
 
-class ObjectLevelType(SQLModel, table=True):
-    level: int = Field(primary_key=True)
+class LevelType(SQLModel, table=True):
+    level: int = Field(primary_key=True, default=None)
     name: str
 
 
 class HouseType(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+    id: int = Field(primary_key=True, default=None)
     name: str
     shortname: str
 
 
-class AddHouseType(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+class AddhouseType(SQLModel, table=True):
+    id: int = Field(primary_key=True, default=None)
     name: str
     shortname: str
 
 
 class AddressObjectType(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+    id: int = Field(primary_key=True, default=None)
     name: str
     shortname: str
-    level: int = Field(foreign_key='objectleveltype.level')
+    level: int = Field(foreign_key='leveltype.level')
 
 
 class ApartmentType(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+    id: int = Field(primary_key=True, default=None)
     name: str
     shortname: str
 
 
 class RoomType(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+    id: int = Field(primary_key=True, default=None)
     nubmer: int
     roomtype: str
 
 
-class ObjectLevels(SQLModel, table=True):
-    objectid: int = Field(primary_key=True)
-    level: int = Field(foreign_key='objectleveltype.level')
+class Reestr(SQLModel, table=True):
+    objectid: int = Field(
+            default=None,
+            primary_key=True
+            )
+    level: int = Field(foreign_key='leveltype.level')
 
 
 class AdministrativeHierarchy(SQLModel, table=True):
+    id: int = Field(primary_key=True, default=None)
     objectid: int = Field(
             default=None,
-            foreign_key='objectlevels.objectid'
+            foreign_key='reestr.objectid'
             )
-    path1: Optional[int] = Field(
-            default=None,
-            foreign_key='objectlevels.objectid'
-            )
-    path2: Optional[int] = Field(
-            default=None,
-            foreign_key='objectlevels.objectid'
-            )
-    path3: Optional[int] = Field(
-            default=None,
-            foreign_key='objectlevels.objectid'
-            )
-    path4: Optional[int] = Field(
-            default=None,
-            foreign_key='objectlevels.objectid'
-            )
-    path5: Optional[int] = Field(
-            default=None,
-            foreign_key='objectlevels.objectid'
-            )
-    path6: Optional[int] = Field(
-            default=None,
-            foreign_key='objectlevels.objectid'
-            )
-    path7: Optional[int] = Field(
-            default=None,
-            foreign_key='objectlevels.objectid'
-            )
-    path8: Optional[int] = Field(
-            default=None,
-            foreign_key='objectlevels.objectid'
-            )
+    path_list: List[int] = Field(sa_column=Column(ARRAY(Integer)))
 
 
 class MunicipalHierarchy(SQLModel, table=True):
+    id: int = Field(primary_key=True, default=None)
     objectid: int = Field(
             default=None,
-            primary_key=True
+            foreign_key='reestr.objectid'
             )
-    path1: Optional[int] = Field(
-            default=None,
-            foreign_key='objectlevels.objectid'
-            )
-    path2: Optional[int] = Field(
-            default=None,
-            foreign_key='objectlevels.objectid'
-            )
-    path3: Optional[int] = Field(
-            default=None,
-            foreign_key='objectlevels.objectid'
-            )
-    path4: Optional[int] = Field(
-            default=None,
-            foreign_key='objectlevels.objectid'
-            )
-    path5: Optional[int] = Field(
-            default=None,
-            foreign_key='objectlevels.objectid'
-            )
-    path6: Optional[int] = Field(
-            default=None,
-            foreign_key='objectlevels.objectid'
-            )
-    path7: Optional[int] = Field(
-            default=None,
-            foreign_key='objectlevels.objectid'
-            )
-    path8: Optional[int] = Field(
-            default=None,
-            foreign_key='objectlevels.objectid'
-            )
+    path_list: List[int] = Field(sa_column=Column(ARRAY(Integer)))
 
 
 class AddressObject(SQLModel, table=True):
+    id: int = Field(primary_key=True, default=None)
     objectid: int = Field(
             default=None,
-            primary_key=True
+            foreign_key='reestr.objectid'
             )
     name: str
     typename: str
-    level: int = Field(foreign_key='objectlevel.level')
+    level: int = Field(foreign_key='leveltype.level')
 
 
 class House(SQLModel, table=True):
+    id: int = Field(primary_key=True, default=None)
     objectid: int = Field(
             default=None,
-            foreign_key='objectlevels.objectid'
+            foreign_key='reestr.objectid'
             )
     housenum: int
     housetype: int = Field(foreign_key='housetype.id')
@@ -145,34 +90,38 @@ class House(SQLModel, table=True):
 
 
 class Carplace(SQLModel, table=True):
+    id: int = Field(primary_key=True, default=None)
     objectid: int = Field(
             default=None,
-            foreign_key='objectlevels.objectid'
+            foreign_key='reestr.objectid'
             )
     number: int
 
 
 class Room(SQLModel, table=True):
+    id: int = Field(primary_key=True, default=None)
     objectid: int = Field(
             default=None,
-            foreign_key='objectlevels.objectid'
+            foreign_key='reestr.objectid'
             )
     number: int
     roomtype: int = Field(foreign_key='roomtype.id')
 
 
 class Apartment(SQLModel, table=True):
+    id: int = Field(primary_key=True, default=None)
     objectid: int = Field(
             default=None,
-            foreign_key='objectlevels.objectid'
+            foreign_key='reestr.objectid'
             )
     number: int
     aparttype: int = Field(foreign_key='apartmenttype.id')
 
 
 class Stead(SQLModel, table=True):
+    id: int = Field(primary_key=True, default=None)
     objectid: int = Field(
             default=None,
-            foreign_key='objectlevels.objectid'
+            foreign_key='reestr.objectid'
             )
     number: int
